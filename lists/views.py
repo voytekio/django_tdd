edf18b8547b19a65d885ecdb82f2f9d6e165a6b3
voytek.io <voytek@voytek.io>
2020-03-29 21:03:24 -0400

    create model for list items and assoc. migration

--- lists/views.py
+++ lists/views.py
@@ -11,6 +11,7 @@ def home_page(request):
     #if request.method == 'POST':
     #    return render(request, 'home.html', {'new_item_text':request.POST['item_text']})
         #return HttpResponse(request.POST['item_text'])
+    print('POST Request included: {}'.format(request.POST))
     return render(request, 'home.html', {'new_item_text':request.POST.get('item_text', '')})
     #return render(request, 'home.html')
     #return HttpResponse('<html><title>To-Do lists</title></html>')
