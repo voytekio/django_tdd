8c91f14a9b64728cff5620085674abf213323e0e
voytek.io <voytek@voytek.io>
2020-04-27 23:24:38 -0400

    added new url to save lists and associated adjustments

--- lists/tests.py
+++ lists/tests.py
@@ -23,23 +23,6 @@ class Test_HomePageTest(TestCase):
         #response_final = response_raw.content.decode('utf8')
         self.assertTemplateUsed(response_raw, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-        #self.assertIn('A new list item', response.content.decode())
-        #self.assertTemplateUsed(response, 'home.html')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
 
 class ListViewTest(TestCase):
     def test_displays_all_items(self):
@@ -56,6 +39,23 @@ class ListViewTest(TestCase):
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+        #self.assertEqual(response.status_code, 302)
+        #self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
--- lists/views.py
+++ lists/views.py
@@ -8,17 +8,12 @@ from lists.models import Item
 
 #home_page = None
 def home_page(request):
-    #pdb.set_trace()
-    #return HttpResponse(request.POST['item_text'])
-    if request.method == 'POST':
-        print('POST Request included: {}'.format(request.POST['item_text']))
-        Item.objects.create(text=request.POST['item_text'])
-        #item_orm = Item()
-        #item_orm.text = request_post.get('item_text', '')
-        #item_orm.save()
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
-    #return HttpResponse('<html><title>To-Do lists</title></html>')
 
 def view_list(request):
     return render(request, 'list.html', {'items':Item.objects.all()})
+
+def new_list(request):
+    print('POST Request included: {}'.format(request.POST['item_text']))
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
